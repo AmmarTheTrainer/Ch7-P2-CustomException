548ed002ad5106e0d82111970918e4c2bdddb4ac
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-08-10 02:16:40 +0500

    first commit from Lahore Markaz

---
+++ Ch7-P2-CustomException/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch7_P2_CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Custom exceptions ");
+
+            Console.ReadLine();
+        }
+    }
+}
