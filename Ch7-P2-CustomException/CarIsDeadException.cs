9aa966ecb16f785783ff058f1babd17dca7d5475
Ammar Innowi <ammar.shaukat@innowi.com>
2018-08-10 16:06:58 +0500

    final commit from head office

--- Ch7-P2-CustomException/CarIsDeadException.cs
+++ Ch7-P2-CustomException/CarIsDeadException.cs
@@ -38,9 +38,28 @@ namespace Ch7_P2_CustomException
 
     #endregion
 
-    
-    
+    #region A little optimized custom .net exception
 
+
+    //public class CarIsDeadException : ApplicationException
+    //{
+    //    private string messageDetails = String.Empty;
+    //    public DateTime ErrorTimeStamp { get; set; }
+    //    public string CauseOfError { get; set; }
+
+    //    public CarIsDeadException() { }
+    //    public CarIsDeadException(string message, string cause, DateTime time) : base(message)
+    //    {
+    //        CauseOfError = cause;
+    //        ErrorTimeStamp = time;
+    //    }
+    //}
+
+    #endregion
+
+    #region A fully optimized .NET custom Exception < without code snippet >
+
+    [Serializable]
     public class CarIsDeadException : ApplicationException
     {
         private string messageDetails = String.Empty;
@@ -48,21 +67,36 @@ namespace Ch7_P2_CustomException
         public string CauseOfError { get; set; }
 
         public CarIsDeadException() { }
-        //public CarIsDeadException(string message, string cause, DateTime time)
-        //{
-        //    messageDetails = message;
-        //    CauseOfError = cause;
-        //    ErrorTimeStamp = time;
-        //}
-        public CarIsDeadException(string message, string cause, DateTime time) : base(message)
-        {
-            CauseOfError = cause;
-            ErrorTimeStamp = time;
-        }
-        //// Override the Exception.Message property.
-        //public override string Message => $"Car Error Message: {messageDetails}";
+        public CarIsDeadException(string message) : base(message)
+        { }
+        public CarIsDeadException(string message , System.Exception inner) : base(message,inner)
+        { }
+
+        protected CarIsDeadException(System.Runtime.Serialization.SerializationInfo info, 
+                                     System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        { }
+        
+        // Any additional custom properties, constructors and data members...
+        
     }
 
+    #endregion
+
+    #region .NET Custom Exception generted through code snippet.
+
+
+    //[Serializable]
+    //public class MyException : Exception
+    //{
+    //    public MyException() { }
+    //    public MyException(string message) : base(message) { }
+    //    public MyException(string message, Exception inner) : base(message, inner) { }
+    //    protected MyException(
+    //      System.Runtime.Serialization.SerializationInfo info,
+    //      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    //}
+
+    #endregion
 
     //class abc : SystemException
     //{
